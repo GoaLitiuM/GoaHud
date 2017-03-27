6b4efadeb1d1cc0e0898ed334d1ced01102cfb7b
GoaLitiuM <goalitium@kapsi.fi>
2017-03-27 06:17:33 +0300

    Glow selected weapon in weapon rack

--- GoaHud/GoaHud_WeaponRack.lua
+++ GoaHud/GoaHud_WeaponRack.lua
@@ -20,8 +20,14 @@ GoaHud_WeaponRack =
 
 		coloredAmmo = false,
 		textColor = Color(255,255,255,255),
+		
+		coloredSelection = false,
+		selectedColor = Color(255,255,255,128),
+		
+		showBackground = true,
+		backgroundColor = Color(0, 0, 0, 64),
 	},
-	optionsDisplayOrder = { "hideMelee", "hideBurstGun", "hideInRace", "hideInWarmup", "coloredAmmo", "textColor"},
+	optionsDisplayOrder = { "hideMelee", "hideBurstGun", "", "hideInRace", "hideInWarmup", "", "coloredAmmo", "textColor", "coloredSelection", "selectedColor", "showBackground", "backgroundColor"},
 	
 	showWeapons = {},
 };
@@ -44,6 +50,18 @@ function GoaHud_WeaponRack:drawOptionsVariable(varname, x, y, optargs)
 	elseif (varname == "hideModes") then
 		options_table = self.options.hideModes
 		id = 105
+	elseif (varname == "textColor") then
+		local optargs = clone(optargs)
+		optargs.enabled = not self.options.coloredAmmo
+		return GoaHud_DrawOptionsVariable(self.options, varname, x + GOAHUD_INDENTATION, y, optargs, "Ammo Text Color")
+	elseif (varname == "selectedColor") then
+		local optargs = clone(optargs)
+		optargs.enabled = not self.options.coloredSelection
+		return GoaHud_DrawOptionsVariable(self.options, varname, x + GOAHUD_INDENTATION, y, optargs)
+	elseif (varname == "backgroundColor") then
+		local optargs = clone(optargs)
+		optargs.enabled = self.options.showBackground
+		return GoaHud_DrawOptionsVariable(self.options, varname, x + GOAHUD_INDENTATION, y, optargs)
 	end
 	
 	local offset = nil
@@ -69,28 +87,30 @@ function GoaHud_WeaponRack:draw()
 	self.showWeapons[2] = not self.options.hideBurstGun
 	
 	local player = getPlayer()
+	local weapon = player.weaponIndexweaponChangingTo
 	local offset_x = 0
 	local weapons_picked = 0
 	local weapon_count = 0
 	local icon_size = 15
 	local font_size = 40
 	local offset_width = icon_size*2 + font_size*0.75*3
+	local border = 5
+	
 	
 	for i in ipairs(weaponDefinitions) do
 		weapon_count = weapon_count + 1
 		if (self.showWeapons[i] and player.weapons[i].pickedup) then weapons_picked = weapons_picked + 1 end
 	end
 	
+	local background_width = (weapons_picked * offset_width) + border*2
+	local background_height = math.max(font_size*0.6, icon_size*2) + border*2
+	
 	offset_x = -weapons_picked * offset_width / 2
 	
-	if (weapons_picked > 0) then
-		local border = 5
-		local background_width = (weapons_picked * offset_width) + border*2
-		local background_height = math.max(font_size*0.6, icon_size*2) + border*2
-		
+	if (weapons_picked > 0 and self.options.showBackground) then
 		nvgBeginPath()
-		nvgFillColor(Color(0, 0, 0, 64))
-		nvgRect(-icon_size-border + offset_x, -background_height/2, background_width, background_height)
+		nvgFillColor(self.options.backgroundColor)
+		nvgRect(-icon_size - border + offset_x, -background_height/2, background_width, background_height)
 		nvgFill()
 	end
 	
@@ -104,6 +124,18 @@ function GoaHud_WeaponRack:draw()
 			local def = weaponDefinitions[i];
 			local ammo = math.min(player.weapons[i].ammo, 999)
 			local color = Color(def.color.r, def.color.g, def.color.b, def.color.a);
+			
+			-- selected weapon glow
+			if (weapon == i) then
+				nvgBeginPath()
+				if (not self.options.coloredSelection) then
+					nvgFillColor(self.options.selectedColor)
+				else
+					nvgFillColor(Color(color.r, color.g, color.b, color.a * 0.6))
+				end
+				nvgRect(offset_x - icon_size - border , -background_height/2, offset_width, background_height)
+				nvgFill()
+			end
 			
 			local weapon_icon
 			if (i == 1 and player.inventoryMelee ~= nil) then
