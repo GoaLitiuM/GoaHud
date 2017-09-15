cb30875a81cd049f5f1a904990cb10b042236d17
GoaLitiuM <goalitium@kapsi.fi>
2017-09-15 05:54:56 +0300

    Update changelogs

--- GoaHud/GoaHud_Version.lua
+++ GoaHud/GoaHud_Version.lua
@@ -19,8 +19,12 @@ end
 --
 
 changelog[30] = "\z
-- BetterGameplay: Added option to disable official item timers during casual duels\n\z
-\z
+- BetterGameplay: Added option to disable official item timers in casual duels\n\z
+- Added support for extended chat color codes from CPMA (^9, ^a, ^z, etc.)\n\z
+- Chat: Use Twitter emojis over Kimi's emojis\n\z
+- Chat: Text is now colored in realtime while typing in chat with color codes\n\z
+- Chat: Added options to disable emojis and colors\n\z
+- Show current version of GoaHud in widget options\n\z
 "
 
 
